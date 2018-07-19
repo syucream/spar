61d44f9d873ee7ccf001369d6c46aaa9243f09e9
syucream <syucream@mercari.com>
2018-07-19 11:45:58 +0900

    Fix .gitignore

---
+++ cmd/jack/jack.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/syucream/jack/src/parser"
+)
+
+func main() {
+	pathToSql := os.Args[1]
+	data, err := ioutil.ReadFile(pathToSql)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stmt := parser.ParseSpannerDDL(strings.NewReader(string(data)))
+	fmt.Println(stmt)
+}
