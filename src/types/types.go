9daeaa2acd7b8fe8806b0feb4f9129e19bdbfcb3
Ryo Okubo <syucream1031@gmail.com>
2018-07-27 22:56:02 +0900

    Add docs

--- src/types/types.go
+++ src/types/types.go
@@ -1,5 +1,6 @@
 package types
 
+// DDStatements has parsed statements.
 type DDStatements struct {
 	CreateDatabases []CreateDatabaseStatement
 	CreateTables    []CreateTableStatement
@@ -9,6 +10,7 @@ type DDStatements struct {
 	DropIndexes     []DropIndexStatement
 }
 
+// Column is a table column.
 type Column struct {
 	Name        string
 	Type        string // BOOL, INT64, ...
@@ -16,28 +18,34 @@ type Column struct {
 	Options     string
 }
 
+// Key is a table key.
 type Key struct {
 	Name  string
 	Order string
 }
 
+// Cluster is a Spanner table cluster.
 type Cluster struct {
 	TableName string
 	OnDelete  string
 }
 
+// StoringClause is a storing clause info.
 type StoringClause struct {
 	ColumnNames []string
 }
 
+// Interleave is a interlive.
 type Interleave struct {
 	TableName string
 }
 
+// Alteration is a alter table operation.
 type Alteration interface {
 	Alter()
 }
 
+// AddColumnTableAlteration is a alter table operation, corresponding to 'ADD COLUMN'.
 type AddColumnTableAlteration struct {
 	Column Column
 }
@@ -45,6 +53,7 @@ type AddColumnTableAlteration struct {
 // Alter satisfies Alteration interface
 func (self *AddColumnTableAlteration) Alter() {}
 
+// DropColumnTableAlteration is a alter table operation, corresponding to 'DROP COLUMN'.
 type DropColumnTableAlteration struct {
 	ColumnName string
 }
@@ -52,6 +61,7 @@ type DropColumnTableAlteration struct {
 // Alter satisfies Alteration interface
 func (self *DropColumnTableAlteration) Alter() {}
 
+// SetTableAlteration is a alter table operation, corresponding to 'SET'.
 type SetTableAlteration struct {
 	OnDelete string
 }
@@ -59,6 +69,7 @@ type SetTableAlteration struct {
 // Alter satisfies Alteration interface
 func (self *SetTableAlteration) Alter() {}
 
+// AlterColumnTypesAlteration is a alter table operation, corresponding to 'ALTER COLUMN'.
 type AlterColumnTypesAlteration struct {
 	ColumnName  string
 	ColumnType  string
@@ -68,6 +79,7 @@ type AlterColumnTypesAlteration struct {
 // Alter satisfies Alteration interface
 func (self *AlterColumnTypesAlteration) Alter() {}
 
+// AlterColumnSetAlteration is a alter table operation, corresponding to 'ALTER COLUMN'.
 type AlterColumnSetAlteration struct {
 	ColumnName string
 	Options    string
@@ -76,10 +88,12 @@ type AlterColumnSetAlteration struct {
 // Alter satisfies Alteration interface
 func (self *AlterColumnSetAlteration) Alter() {}
 
+// CreateDatabaseStatement is a 'CREATE DATABASE' statement info.
 type CreateDatabaseStatement struct {
 	DatabaseId string
 }
 
+// CreateTableStatement is a 'CREATE TABLE' statement info.
 type CreateTableStatement struct {
 	TableName   string
 	Columns     []Column
@@ -87,6 +101,7 @@ type CreateTableStatement struct {
 	Cluster     Cluster
 }
 
+// CreateIndexStatement is a 'CREATE INDEX' statement info.
 type CreateIndexStatement struct {
 	IndexName     string
 	Unique        string
@@ -97,15 +112,18 @@ type CreateIndexStatement struct {
 	Interleaves   []Interleave
 }
 
+// AlterTableStatement is a 'ALTER TABLE' statement info.
 type AlterTableStatement struct {
 	TableName  string
 	Alteration Alteration // Get concrete type yourself
 }
 
+// DropTableStatement is a 'DROP TABLE' statement info.
 type DropTableStatement struct {
 	TableName string
 }
 
+// DropIndexStatement is a 'DROP INDEX' statement info.
 type DropIndexStatement struct {
 	IndexName string
 }
